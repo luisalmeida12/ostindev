3c2a1352b07adb87f9b27f7e4e689beed1fd992c
Luis Almeida <info@luis-almeida.com>
2019-08-09 01:21:03 +0100

    Initial commit

---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const DevController = require('./controllers/DevController');
+
+routes.post('/devs', DevController.store);
+
+module.exports = routes;
